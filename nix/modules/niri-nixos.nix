ada2903e5a5f86146e8420c93cd0467903d75cc5
kagurazakei <maotsugiri@gmail.com>
2026-02-27 09:52:10 +0630

    fix: anonymous lambda

--- nix/modules/niri-nixos.nix
+++ nix/modules/niri-nixos.nix
@@ -1,96 +1,75 @@
+{ overlay }:
 {
   pkgs,
   config,
-  overlay,
   lib,
   ...
 }:
 
 let
   cfg = config.programs.niri;
-  inherit (lib)
-    mkIf
-    mkMerge
-    mkEnableOption
-    mkPackageOption
-    ;
 in
 {
   options.programs.niri = {
-    enable = mkEnableOption "Custom Niri with overlay";
+    enable = lib.mkEnableOption "Custom Niri with overlay";
 
-    package = mkPackageOption pkgs "niri" { };
+    withUWSM = lib.mkEnableOption "Enable UWSM support";
 
-    useThunar = mkEnableOption "Use Thunar integration" // {
-      default = true;
-    };
-
-    withUWSM = mkEnableOption "Enable UWSM support" // {
-      default = true;
-    };
-
-    withXDG = mkEnableOption "Enable XDG portal support" // {
+    withXDG = lib.mkEnableOption "Enable XDG portal support" // {
       default = true;
     };
   };
 
-  config = mkIf cfg.enable (mkMerge [
+  config = lib.mkIf cfg.enable {
 
-    {
-      nixpkgs.overlays = [ overlay ];
+    # Apply overlay
+    nixpkgs.overlays = [ overlay ];
 
-      # Use overlay-provided niri
-      programs.niri.package = cfg.package;
+    # Use overlay niri
+    programs.niri.package = pkgs.niriPackages.niri;
 
-      services.displayManager.sessionPackages = [ cfg.package ];
+    # Make session available
+    services.displayManager.sessionPackages = [
+      pkgs.niriPackages.niri
+    ];
 
-      security.polkit.enable = true;
+    # Required basics
+    security.polkit.enable = true;
+    programs.dconf.enable = lib.mkDefault true;
+    services.gnome.gnome-keyring.enable = lib.mkDefault true;
 
-      programs.dconf.enable = lib.mkDefault true;
-
-      services.gnome.gnome-keyring.enable = lib.mkDefault true;
-
-      services.xserver.desktopManager.runXdgAutostartIfNone = lib.mkDefault true;
-
-      services.dbus.packages = mkIf cfg.useThunar [ pkgs.thunar ];
-    }
-
-    (mkIf cfg.withUWSM {
-      programs.uwsm = {
-        enable = true;
-        waylandCompositors = {
-          niri = {
-            prettyName = "Niri";
-            comment = "Scrollable-tiling Wayland compositor";
-            binPath = "${cfg.package}/bin/niri-session";
-          };
-        };
+    # -------------------------
+    # Optional UWSM
+    # -------------------------
+    programs.uwsm = lib.mkIf cfg.withUWSM {
+      enable = true;
+      waylandCompositors.niri = {
+        prettyName = "Niri";
+        comment = "Scrollable-tiling Wayland compositor";
+        binPath = "${pkgs.niriPackages.niri}/bin/niri-session";
       };
-    })
+    };
 
-    (mkIf cfg.withXDG {
-      xdg.portal = {
-        enable = true;
+    # -------------------------
+    # Optional XDG Portal
+    # -------------------------
+    xdg.portal = lib.mkIf cfg.withXDG {
+      enable = true;
 
-        extraPortals = with pkgs; [
-          xdg-desktop-portal-gtk
-          xdg-desktop-portal-gnome
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+      ];
+
+      config.niri = lib.mkDefault {
+        default = [
+          "gtk"
+          "gnome"
         ];
-
-        config = {
-          niri = lib.mkDefault {
-            default = [
-              "gtk"
-              "gnome"
-            ];
-
-            "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-            "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
-            "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-          };
-        };
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Notification" = [ "gtk" ];
+        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
       };
-    })
-
-  ]);
+    };
+  };
 }
